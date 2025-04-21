3f9a51f0f28f79cef909856b53f9760742a16394
User <user@example.com>
2025-04-21 16:38:29 -0700

    added app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ScientificMethod from './components/ScientificMethod'
+
+
+function App() {
+  return (
+      <div className="App">
+        <ScientificMethod />
+      </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/ScientificMethod.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+
+
+
+const ScientificMethod = () => {
+
+
+	return (
+        <div></div>
+    )
+};
+
+
+export default ScientificMethod;
